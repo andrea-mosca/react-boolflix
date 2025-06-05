00e16bd70f7bae0f2ba38c3d1e8f5274f0588efc
andrea-mosca <andrea.moscateli@gmail.com>
2025-06-05 18:50:28 +0200

    install font awesome

--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -9,10 +9,21 @@ export default function Card({
     <div className="card">
       <img src={image} alt={title} className="card-img-top" />
       <div className="card-body">
-        <h5>title:{title}</h5>
-        <div>original title:{originalTitle}</div>
-        <div>original language:{language}</div>
-        <div>valutation:{valutation}</div>
+        <span className="fw-bolder">Title: </span>
+        <span>{title}</span>
+        <div className="mt-1">
+          <span className="fw-bolder">Original title: </span>
+          <span>{originalTitle}</span>
+        </div>
+        <div className="mt-1">
+          <span className="fw-bolder">Original language: </span>
+
+          <span>{language}</span>
+        </div>
+        <div className="mt-1">
+          <span className="fw-bolder">Valutation:</span>{" "}
+          <span>{valutation}</span>
+        </div>
       </div>
     </div>
   );
